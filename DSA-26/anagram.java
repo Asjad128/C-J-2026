39f9cdcb4db27d1ae1c1d4098d006ba90ca4f971
Asjad128 <abdulgaffoorasjad06@gmail.com>
2026-08-20 00:42:11 +0530

    Refactor anagram logic to improve clarity and correctness

--- DSA-26/anagram.java
+++ DSA-26/anagram.java
@@ -1,23 +1,34 @@
 
 
 public class anagram {
+
+    static boolean are_anagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] frequency = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            frequency[str1.charAt(i) - 'a']++;
+            frequency[str2.charAt(i) - 'a']--;
+        }
+        for (int count : frequency) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
         String str1 = "silentc";
         String str2 = "litenw";
 
-        if (str1.length() != str2.length()) {
-            System.out.println("Not anagrams");
-            return;
-        }
-
-        int weight=0;
-        for (int i = 0; i < str1.length(); i++) {
-            weight += str1.charAt(i) - str2.charAt(i);
-        }
-        if (weight == 0) {
-            System.out.println("Anagrams");
-        } else {    
-            System.out.println("Not anagrams");
+        if (are_anagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
         }
     }
 
