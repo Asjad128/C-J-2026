bfc5cca3eb6c8b24d8ece4ff5a87868ecb4ae0d2
Asjad128 <abdulgaffoorasjad06@gmail.com>
2026-08-20 00:24:22 +0530

    Refactor duplicates method to use ArrayList for duplicate detection

--- DSA-26/containsduplicate.java
+++ DSA-26/containsduplicate.java
@@ -1,21 +1,26 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class containsduplicate{
+
+    static boolean duplicates(int[] arr){
+
+        List<Integer> list = new ArrayList<>();
+        for (int i: arr){
+            if (list.contains(i)){
+                return true;
+            }
+            list.add(i);
+        }
+
+        return false;
+    }
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5};
-        int check=0;
-        Set<Integer> set= new HashSet<>();
-        for (int i: arr){
-            if (set.contains(i)){check+=1;}
-            set.add(i);
-        }
-        if (check==0){
-            System.out.println(false);
-        }
-        else{
-            System.out.println(true); 
-        }
+        int[] arr2={1,2,3,4,5,1};
+        
+        System.out.println(duplicates(arr));
+        System.out.println(duplicates(arr2));
     }
 }
 
