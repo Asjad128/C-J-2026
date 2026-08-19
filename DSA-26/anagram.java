dba1196236d134ff0d1da1dd346a544dc8399710
Asjad128 <abdulgaffoorasjad06@gmail.com>
2026-08-20 00:03:02 +0530

    Refactor anagram logic to use character weight comparison

--- DSA-26/anagram.java
+++ DSA-26/anagram.java
@@ -1,15 +1,23 @@
-import java.util.Arrays;
 
 
 public class anagram {
     public static void main(String[] args) {
-        String str1 = "silent";
-        String str2 = "listen";
+        String str1 = "silentc";
+        String str2 = "litenw";
 
-        if (Arrays.equals(str1.chars().sorted().toArray(), str2.chars().sorted().toArray()) && str1.length() == str2.length()) {
-            System.out.println(str1 + " and " + str2 + " are anagrams.");
-        } else {
-            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        if (str1.length() != str2.length()) {
+            System.out.println("Not anagrams");
+            return;
+        }
+
+        int weight=0;
+        for (int i = 0; i < str1.length(); i++) {
+            weight += str1.charAt(i) - str2.charAt(i);
+        }
+        if (weight == 0) {
+            System.out.println("Anagrams");
+        } else {    
+            System.out.println("Not anagrams");
         }
     }
 
