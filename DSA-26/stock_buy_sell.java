ea53dbf7dbeb36cbcf7ec90e4e7791043d47e0c3
Asjad128 <abdulgaffoorasjad06@gmail.com>
2026-08-21 00:39:19 +0530

    Refactor stock_buy_sell.java to improve price difference calculation logic

--- DSA-26/stock_buy_sell.java
+++ DSA-26/stock_buy_sell.java
@@ -1,20 +1,37 @@
 
 public class stock_buy_sell {
     public static void main(String args[]){
-        int[] prices = {7, 1, 5, 3, 6, 4};
-        int min=prices[0], max=prices[0];
+        
+        int[] prices={7,1,4,5,6,2,8, 2};
+        
+        int min=prices[0];
+        int diff=0;
 
-        for (int i=0; i<prices.length; i++){
-            for(int j=i+1; j<prices.length; j++){
-
-                if (prices[j] - prices[i] > max-min){
-                    min=prices[i];
-                    max=prices[j];
-                }
-
+        for (int i=0; i<prices.length-1; i++){
+            System.out.println(i+"="+prices[i]);
+            
+            if(min>prices[i]){
+                min=prices[i];
+                System.out.println(i+"1-min: "+min);
+                continue;
             }
+
+            if(prices[i+1] - prices[i] > prices[i] - min){
+                min=prices[i];
+                diff=prices[i+1]-min;
+                System.out.println(i+"2-min: "+min);
+                continue;
+            }
+
+            
+
+            if(prices[i]-min > diff){
+                diff = prices[i]-min;
+                System.out.println(i+"3-min: "+min);
+            }
+
         }
         
-        System.out.println(max-min);
+        System.out.println(diff);
     }
 }
